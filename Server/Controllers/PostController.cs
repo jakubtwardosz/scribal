44bcb2397328dc921407d51cfb314b72420f19b2
Jakub Twardosz <jakubtwardosz@protonmail.com>
2023-08-25 12:08:10 +0200

    Display data on the client

--- Server/Controllers/PostController.cs
+++ Server/Controllers/PostController.cs
@@ -11,8 +11,15 @@ namespace scribal.Server.Controllers
         {
             new Post
             {
+                Id = 1,
                 Title = "Title",
-                Content = "Content"
+                Content = "Expedita non rem delectus cumque ab architecto repellendus voluptas. Consequatur eos similique dicta. Ea iure vitae non quos. Cumque magni ullam sint optio sit molestias. Quo quis consequatur recusandae praesentium debitis harum beatae. Doloribus nisi error consectetur aut eveniet tempora omnis nihil.",
+            },
+            new Post
+            {
+                Id = 2,
+                Title = "Title",
+                Content = "Expedita non rem delectus cumque ab architecto repellendus voluptas. Consequatur eos similique dicta. Ea iure vitae non quos. Cumque magni ullam sint optio sit molestias. Quo quis consequatur recusandae praesentium debitis harum beatae. Doloribus nisi error consectetur aut eveniet tempora omnis nihil.",
             }
         };
 
